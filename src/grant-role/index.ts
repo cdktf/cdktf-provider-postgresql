7101f7886239924fb89e1eb4d4789f6b24a0bd73
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-05 11:21:51 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/grant-role/index.ts
+++ src/grant-role/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/cyrilgdn/postgresql/1.22.0/docs/resources/grant_role
 // generated from terraform resource schema
 
